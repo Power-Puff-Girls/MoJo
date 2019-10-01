9c5acdcb36239148e6c0cfb6cdcb44c6294052c6
littl3birdy <ashlynmmorin@gmail.com>
2019-10-01 09:52:05 -0700

    Raven and Ashlyn Working on events in data file

--- js/data.js
+++ js/data.js
@@ -1,28 +1,37 @@
 // local storage js here
 'use strict';
 
-Entry.allEntries =[];
+Entry.allEntries = [];
 
 //constructor function
-function Entry(mood, text){
+function Entry(mood, text, day) {
   this.mood = mood;
   this.text = text;
+  this.day = day;
+  Entry.allEntries.push(this);
 }
 
 var addEntry = document.getElementById('txtBtn');
 addEntry.addEventListener('submit', addNewEntry);
 
-//TODO: hide entry box after click submit; show entry logged message
-
-
 //local storage for journal entries
-function addNewEntry(){
-  var json = JSON.stringify(addEntry);
+function addNewEntry() {
+  var myMood= document.getElementById('moodChoice');
+  new Entry()
+  var json = JSON.stringify(Entry.allEntries);
   localStorage.setItem('entry', json);
+  renderSubmit();
+}
+//TODO: hide entry box after click submit; show entry logged message
+function renderSubmit() {
+  var journal = document.getElementById('newEntry');
+  journal.style.display = 'none';
+  var submitMessage = document.getElementById('submitMessage');
+  submitMessage.style.display = 'block';
 }
 
 function getEntry() {
-  if(localStorage.entry){
+  if (localStorage.entry) {
     var entries = localStorage.getItem('entry');
     var parsed = JSON.parse(entry);
 
